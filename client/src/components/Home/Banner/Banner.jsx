3525384c340ce3c37ae7f60722bae914a229f551
Rahul Kumar Yadav <jgrrahul333@gmail.com>
2023-07-14 23:46:32 +0530

    Rahul third change

--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,23 @@
 import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png";
 const Banner = () => {
-    return <div>Banner</div>;
+    return <div className="hero-banner">
+        <div className="content">
+          <div className="text-content">
+            <h1>SALES</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic aspernatur accusamus et. Cum, nisi. Placeat deserunt provident maxime sit natus ullam, corporis voluptatem, ratione quaerat nemo dicta illo unde voluptas!
+                Autem accusantium quisquam, eaque voluptate, laborum temporibus dignissimos dolorem labore sint ut dolorum suscipit tenetur aliquid maxime, corrupti doloribus architecto adipisci laudantium voluptatibus exercitationem fuga obcaecati repellendus necessitatibus. Dolore, aliquam!
+                Minus, labore asperiores tempora, incidunt sed magni officia distinctio numquam enim, est natus beatae iusto corporis. Aliquid sunt dolor, repudiandae laborum est recusandae iure culpa labore minima vero sed ullam?
+            </p>
+            <div className='ctas'>
+                <div className="banner-cta">Read More</div>
+                <div className="banner-cta v2">Shop Now</div>
+            </div>
+          </div>
+          <img className="banner-img" src={BannerImg} alt="" />
+        </div>
+    </div>;
 };
 
 export default Banner;
