0af9cd600afb8fc847e054fe0ea22579dc8c0439
Abhishek Rai <raia2534@gmail.com>
2023-07-18 03:26:30 +0530

    A9 Shopping Cart

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
-import {useEffect, useState, useContext } from "react";  
-import { useNavigate } from "react-router-dom";  
+import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { TbSearch } from "react-icons/tb"; 
-import { CgShoppingCart} from "react-icons/cg"; 
-import { AiOutlineHeart } from "react-icons/ai" 
+import { TbSearch } from "react-icons/tb";
+import { CgShoppingCart } from "react-icons/cg";
+import { AiOutlineHeart } from "react-icons/ai"
 
 import Search from "./Search/Search";
 import Cart from "../Cart/Cart";
@@ -13,37 +13,43 @@ import "./Header.scss";
 
 const Header = () => {
     const [scrolled, setScrolled] = useState(false);
-    const handleScroll = () =>{
+    const [showCart, setShowCart] = useState(false);
+    const handleScroll = () => {
         const offset = window.scrollY
-       if(offset > 200){
-        setScrolled(true);
-       }else{
-        setScrolled(false);
-       }
+        if (offset > 200) {
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
     };
 
 
-    useEffect(() =>{
+    useEffect(() => {
         window.addEventListener("scroll", handleScroll)
     }, [])
-    return <header className={`main-header ${scrolled ? 'sticky-header' : ''}`}>
-        <div className="header-content">
-            <ul className="left">
-                <li>Home</li>
-                <li>About</li>
-                <li>Categories</li>
-            </ul>
-            <div className="center">AUDIO VERSE</div>
-            <div className="right">
-                <TbSearch />
-                <AiOutlineHeart />
-                <span className="cart-icon">
-                    <CgShoppingCart />
-                    <span>5</span>
-                </span>
-            </div>
-        </div>
-    </header>;
+    return (
+        <>
+            <header className={`main-header ${scrolled ? 'sticky-header' : ''}`}>
+                <div className="header-content">
+                    <ul className="left">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Categories</li>
+                    </ul>
+                    <div className="center">AUDIO VERSE</div>
+                    <div className="right">
+                        <TbSearch />
+                        <AiOutlineHeart />
+                        <span className="cart-icon" onClick={() => setShowCart(true)}>
+                            <CgShoppingCart />
+                            <span>5</span>
+                        </span>
+                    </div>
+                </div>
+            </header>
+            {showCart && <Cart setShowCart={setShowCart} />}
+        </>
+    );
 };
 
 export default Header;
